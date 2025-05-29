1beaf9744dfb24d9a09e09de4116c23af06b5f43
vutranquangminh <19112004vutranquangminh@gmail.com>
2025-05-29 19:41:13 +0700

    done in 45 minnutes

---
+++ 003-array/003-easy.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = []
+        for i in range(len(nums)):
+                try:
+                    for j in range(i + 1,len(nums)):
+                        if nums[i] + nums[j] == target:
+                            arr.append(i)
+                            arr.append(j)
+                            return arr
+                except:
+                    return False
+        
+        return arr
