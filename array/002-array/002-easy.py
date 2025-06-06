e008a04b04d4e0f15b29807b8280678f296d1d1b
vutranquangminh <19112004vutranquangminh@gmail.com>
2025-06-07 01:09:27 +0700

    update fastest  solution

--- array/002-array/002-easy.py
+++ array/002-array/002-easy.py
@@ -2,24 +2,11 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        s_dict = {}
-        t_dict = {}
-        for m in s:
-            if m not in s_dict:
-                s_dict[m] = 1
-            else:
-                s_dict[m] += 1
         
-        for n in t:
-            if n not in t_dict:
-                t_dict[n] = 1
-            else:
-                t_dict[n] += 1
-
-        try: 
-            for i in s_dict:
-                if s_dict[i] != t_dict[i]:
-                    return False
-        except:
-            return False
-        return True
+        s = s.lower()
+        t = t.lower()
+        count = [0] *26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        return all( c==0 for c in count )
