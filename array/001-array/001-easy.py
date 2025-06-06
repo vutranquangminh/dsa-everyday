264f3f2888f33aed466cc3ef113a76e059f74243
vutranquangminh <19112004vutranquangminh@gmail.com>
2025-06-07 00:29:22 +0700

    update fastest  solution

--- array/001-array/001-easy.py
+++ array/001-array/001-easy.py
@@ -1,7 +1,3 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        for num in nums:
-            check = nums.count(num)
-            if check > 1:
-                return True
-        return False
+        return len(set(nums)) != len(nums)
