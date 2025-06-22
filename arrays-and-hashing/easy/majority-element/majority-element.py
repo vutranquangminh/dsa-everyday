93e3697347a14eabe6ffb69579fd8dc35c1f3012
vutranquangminh <19112004vutranquangminh@gmail.com>
2025-06-22 21:34:56 +0700

    done in 5 minutes

---
+++ arrays-and-hashing/easy/majority-element/majority-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        n = len(nums)
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+            if count[num] > n // 2:
+                return num
