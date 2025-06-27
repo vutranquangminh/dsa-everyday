2567743948d7507b8463d93d37b3c18e19c31f9e
vutranquangminh <19112004vutranquangminh@gmail.com>
2025-06-27 13:42:06 +0700

    done in 3 minutes

---
+++ hackerrank/warmup/plus-minus/plus-minus-solution.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+
+def plusMinus(arr):
+    # Write your code here
+    num = len(arr)
+    nagative = []
+    positive = []
+    zero = []
+    for i in arr:
+        if i < 0:
+            nagative.append(i)
+        elif i > 0:
+            positive.append(i)
+        else:
+            zero.append(i)
+            
+    print(f'{(len(positive)/num):.6f}')
+    print(f'{(len(nagative)/num):.6f}')
+    print(f'{(len(zero)/num):.6f}')
+
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
