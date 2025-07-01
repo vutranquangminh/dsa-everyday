b89869f4d10e8dd8f6c69682f060d1083c58b009
vutranquangminh <19112004vutranquangminh@gmail.com>
2025-07-01 23:22:17 +0700

    done in 7 minutes

---
+++ hackerrank/implementation/day-of-the-programmer/day-of-the-programmer-solution.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+def dayOfProgrammer(year):
+    if year == 1918:
+        return f"26.09.{year}"
+    elif (year < 1918 and year % 4 == 0) or (year > 1918 and (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0))):
+        return f"12.09.{year}"
+    else:
+        return f"13.09.{year}"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
